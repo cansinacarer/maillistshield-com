47d594e1ef81bd5e4587b54dda26ea6fb0e7549e
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-04-06 23:44:20 +0000

    feat: add devcontainers
    We now have 3 devcontainers:
    1. Flask Application
    2. Postgres Database
    3. pgAdmin

---
+++ .devcontainer/postCreateCommands.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "--------------------------------"
+echo "You can view the local test database with pgAdmin at http://localhost:5050/"
+echo "pgAdmin username: admin@admin.com, password: admin"
+ecgo "The test database is automatically added as a server in pgAdmin."
+echo "On first run, when pgAdmin prompts you for the database password, type in 'password'."
+echo "--------------------------------"
