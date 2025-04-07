f5db38fe217d40526140c78585dc1027677018b9
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-04-07 05:36:24 +0000

    style: add more line breaks in the postCreateCommands.sh messages

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-echo -e "--------------------------------\n"
+echo -e "\n--------------------------------\n"
 echo -e "You can view the local test database with pgAdmin at \e[1;31mhttp://localhost:5050/\e[0m"
 echo -e "The local test database is automatically added as a server in pgAdmin.\n"
 echo -e "pgAdmin username: \e[1;31madmin@admin.com\e[0m, password: \e[1;31madmin\e[0m"
 echo -e "On first run, when pgAdmin prompts you for the database password, use \e[1;31mpassword\e[0m"
-echo -e "\n--------------------------------"
+echo -e "\n--------------------------------\n"
