6107321db3ae27e9384fecea470ffb26c05f0c79
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-04-19 04:19:02 +0000

    test: add a tess for an error page response

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,3 +2,9 @@ def test_home_page(client):
     """Test that the home page loads correctly"""
     response = client.get("/", follow_redirects=True)
     assert response.status_code == 200
+
+
+def test_error_page(client):
+    """Test that the error pages loads correctly"""
+    response = client.get("/some-non-existent-directory", follow_redirects=True)
+    assert response.status_code == 404
